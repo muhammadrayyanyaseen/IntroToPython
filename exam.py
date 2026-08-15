c11cea0ea640ddd2ddb7a1c9339ab7579afc4f28
muhammadrayyanyaseen <muhammadrayyanr62@gmail.com>
2026-08-15 17:21:08 +0500

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,12 @@
+name = (input("What's your name : "))
+
+if name == "Rayyan\n" :
+    print("Yaseen")
+elif name == "Huzaifa\n" : 
+    print("Yaseen")
+elif name == "Fatima\n" :
+    print("Yaseen")
+elif name == "Asma\n" :
+    print("Yaseen")
+else :
+    print ("Who??")
---
+++ formating.py
@@ -0,0 +1,26 @@
+'''# Create variables for your name and age, then use an f-string to print:
+# "My name is [name] and I am [age] years old."
+name = "Alice"
+age = 25
+# Your code here
+print(f"My name is {name} and I am {age} years old")'''
+
+
+'''# Calculate and print the area of a circle with radius 5
+# Use f-string to display: "A circle with radius 5 has an area of [area]"
+radius = 5
+pi = 3.14159
+# Your code here
+area_of_circle = print( 2 *( pi * radius) )
+area = ( 2 * pi * radius) 
+print(f"A circle with radius 5 has an area of " ,[area])'''
+
+
+# Create variables for city, temperature, and unit
+# Print: "The temperature in [city] is [temperature]°[unit]"
+city = "London"
+temperature = 18
+unit = "C"
+# Your code here
+city_temperature_unit =  "The temperature in "+ city +""+ "is" +""+ str(temperature) + " "+unit
+print(city_temperature_unit)
